1fc06a4bf7147d80d5c0c2df55ff34af32477bf6
JessePecar <JPecar19@gmail.com>
2025-03-15 12:40:26 -0400

    Adding new components to hold the roles list and details so we can start editing roles

---
+++ src/app/settings/roles/details/role-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-details',
+  imports: [],
+  template: `
+    <p>
+      role-details works!
+    </p>
+  `,
+  styles: ``
+})
+export class RoleDetailsComponent {
+
+}
---
+++ src/app/settings/roles/list/role-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-list',
+  imports: [],
+  template: `
+    <p>
+      role-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class RoleListComponent {
+
+}
