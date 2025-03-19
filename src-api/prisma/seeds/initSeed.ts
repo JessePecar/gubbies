f419f920ff606d0ed8c7792c05f61849b8bc879f
jpecar <JPecar@Driverse.com>
2025-03-18 22:35:25 -0400

    Starting to use prisma for the data seeding into the database

---
+++ src-api/prisma/seeds/initSeed.ts
@@ -0,0 +1,86 @@
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+async function main() {
+  client.$connect;
+
+  var permissions = await client.permissions.findMany();
+  if (!permissions || permissions.length < 1) {
+    console.log("Adding permissions to the db");
+    await client.permissions.createMany({
+      data: [
+        {
+          id: 1,
+          name: 'APPLICATION_LOGIN',
+        },
+        {
+          id: 2,
+          name: 'INVENTORY',
+        },
+        {
+          id: 3,
+          name: 'INVENTORY_ADJUSTMENTS',
+        },
+        {
+          id: 4,
+          name: 'INVENTORY_COUNTS',
+        },
+        {
+          id: 5,
+          name: 'PRICING',
+        },
+        {
+          id: 6,
+          name: 'PROMOTIONS',
+        },
+        {
+          id: 7,
+          name: 'REPORTS',
+        },
+        {
+          id: 8,
+          name: 'REPORTS_INVENTORY',
+        },
+        {
+          id: 9,
+          name: 'REPORTS_PRICING',
+        },
+        {
+          id: 10,
+          name: 'REPORTS_PROMOTIONS',
+        },
+        {
+          id: 11,
+          name: 'SETTINGS',
+        },
+        {
+          id: 12,
+          name: 'EDIT_USER',
+        },
+        {
+          id: 13,
+          name: 'CREATE_USER',
+        },
+        {
+          id: 14,
+          name: 'EDIT_ROLE',
+        },
+        {
+          id: 15,
+          name: 'CREATE_ROLE',
+        },
+      ],
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await client.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await client.$disconnect()
+    process.exit(1)
+  })
