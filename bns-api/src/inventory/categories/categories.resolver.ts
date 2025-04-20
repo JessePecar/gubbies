0444f5385d09c427a2a3c765fbcfe1f28a65193e
jpecar <JPecar@Driverse.com>
2025-04-20 08:11:20 -0400

    Doing a lot more of the work around the api changes for the category update

--- bns-api/src/inventory/categories/categories.resolver.ts
+++ bns-api/src/inventory/categories/categories.resolver.ts
@@ -1,4 +1,36 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Resolver } from '@nestjs/graphql';
+import { CreateCategoryInput, CreateFamilyInput, CreateSubcategoryInput } from 'src/graphql.schema';
+import { CategoriesService } from 'src/inventory/categories/categories.service';
 
 @Resolver('Categories')
-export class CategoriesResolver {}
+export class CategoriesResolver {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  async getCategories() {
+    return this.categoriesService.getCategories();
+  }
+
+  async getCategoryByCode(@Args('code') code: string) {
+    return this.categoriesService.getCategoryByCode(code);
+  }
+
+  async getSubcategories(@Args('categoryCode') categoryCode: string) {
+    return this.categoriesService.getSubcategories(categoryCode);
+  }
+
+  async getFamilies(@Args('subcategoryCode') subcategoryCode: string) {
+    return this.categoriesService.getFamilies(subcategoryCode);
+  }
+
+  async upsertCategory(@Args('upsertCategory') upsertCategory: CreateCategoryInput) {
+    return this.categoriesService.upsertCategory(upsertCategory);
+  }
+
+  async upsertSubcategory(@Args('upsertSubcategory') upsertSubcategory: CreateSubcategoryInput) {
+    return this.categoriesService.upsertSubcategory(upsertSubcategory);
+  }
+
+  async upsertFamily(@Args('upsertFamily') upsertFamily: CreateFamilyInput) {
+    return this.categoriesService.upsertFamily(upsertFamily);
+  }
+}
