fedd48a161bc0c601977cc9ffa74b5bfb5721651
jpecar <JPecar@Driverse.com>
2025-05-31 23:49:08 -0400

    late night changes

--- apis/apps/auth-api/src/user/user.controller.ts
+++ apis/apps/auth-api/src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from '@auth/user/user.service';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  async getUser(@Param('id') id: number) {
+    return this.userService.getUserById(id);
+  }
+}
