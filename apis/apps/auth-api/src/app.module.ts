547b6f466bc1e2ac73b8f33ee791559495334412
JessePecar <JPecar19@gmail.com>
2025-06-13 11:58:23 -0400

    adding some caching to the auth api

--- apis/apps/auth-api/src/app.module.ts
+++ apis/apps/auth-api/src/app.module.ts
@@ -13,6 +13,8 @@ import { AuthRepositoryModule, AuthClientService } from '@core/repository';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthConstants } from '@core/constants';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
 
 @Module({
   imports: [
@@ -30,6 +32,26 @@ import { AuthConstants } from '@core/constants';
       global: true,
       secret: AuthConstants.secret,
       signOptions: { expiresIn: '14d' },
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => {
+        return {
+          /* Time to live will be 14 days to match the token expiration */
+          ttl:
+            1000 /* Milliseconds */ *
+            60 /* Seconds */ *
+            60 /* Minutes */ *
+            24 /* Hours */ *
+            14 /* Days */,
+          stores: [
+            // new Keyv({
+            //   store: new CacheableMemory(),
+            // }),
+            createKeyv(process.env.REDIS_URL),
+          ],
+        };
+      },
     }),
   ],
   controllers: [
--- apis/apps/auth-api/src/auth/auth.controller.ts
+++ apis/apps/auth-api/src/auth/auth.controller.ts
@@ -11,17 +11,54 @@ import {
   Get,
   Headers,
   HttpCode,
+  HttpException,
+  HttpStatus,
   Post,
+  Query,
+  Res,
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Cache } from 'cache-manager';
+import { v4 as uuid } from 'uuid';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private authService: AuthService,
     private jwtService: JwtService,
+    private cacheManager: Cache,
   ) {}
+
+  // Get the cached key for the login, which will be the token, which will be validated on grab
+  @Get('cached')
+  async getCachedLogin(@Query('key') key: string) {
+    const cachedObject = await this.cacheManager.get<string>(key);
+
+    // Cached object was found, will now validate
+    if (cachedObject) {
+      const userClaims = await this.authService.verifyUser(cachedObject);
+
+      if (userClaims) {
+        return { token: cachedObject };
+      }
+    }
+
+    return new UnauthorizedException('Cached key is no longer valid');
+  }
+
+  @Get('invalidateKey')
+  @HttpCode(200)
+  async invalidateKey(@Query('key') key: string) {
+    const isDeletedSuccess = await this.cacheManager.del(key);
+
+    if (!isDeletedSuccess) {
+      return new HttpException(
+        'Key was unable to be invalidated',
+        HttpStatus.EXPECTATION_FAILED,
+      );
+    }
+  }
 
   @Post()
   @HttpCode(200)
@@ -39,7 +76,12 @@ export class AuthController {
       authClaims /** TODO: Setup the token options **/,
     );
 
-    return { token: generatedToken };
+    // Create the session id and set the token for the value in the cache
+    const sessionId = uuid();
+
+    await this.cacheManager.set(sessionId, generatedToken);
+
+    return { sessionId, token: generatedToken };
   }
 
   @Get()
